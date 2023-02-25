ebe55efabedbc8260feae7a725f23a2a88a5f801
Yuqing Wang <yuqingwang42@gmail.com>
2023-02-25 16:33:31 +0000

    added show/hide buttons

--- App.js
+++ App.js
@@ -1,1 +1,13 @@
-const myButton = document.querySelector('#myButton');
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+
+const content = document.getElementById('content');
+const answer = document.getElementById('answer');
+
+button1.addEventListener('click', function() {
+  content.classList.toggle('hidden');
+});
+
+button2.addEventListener('click', function() {
+  answer.classList.toggle('hidden');
+});
