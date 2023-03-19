bebcda7c99c0a20340b6ae807e23523f67e38f78
Yuqing Wang <bibolikepeaches@gmail.com>
2023-03-19 23:26:15 +0000

    initial commit

---
+++ App.js
@@ -0,0 +1,42 @@
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+
+const content = document.getElementById('content');
+const answer = document.getElementById('answer');
+
+button1.addEventListener('click', function() {
+  content.classList.toggle('hidden');
+});
+
+button2.addEventListener('click', function() {
+  answer.classList.toggle('hidden');
+});
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
